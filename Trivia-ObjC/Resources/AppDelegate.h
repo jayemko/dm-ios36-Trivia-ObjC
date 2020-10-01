cdfd4986dce8493011478f5034078db498d5f601
Jason Koceja <jason@koceja.com>
2020-10-01 12:58:39 -0700

    initial commit

---
+++ Trivia-ObjC/Resources/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Trivia-ObjC
+//
+//  Created by Jason Koceja on 10/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Trivia-ObjC/Resources/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  Trivia-ObjC
+//
+//  Created by Jason Koceja on 10/1/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
